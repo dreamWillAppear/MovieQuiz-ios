d51e7e4162de58199f93a1d99b900acb7d7624f7
Shestakov Bogdan <dreamlogin@internet.ru>
2023-11-17 10:11:12 +0300

    Внутри замыканий добавлена слабая ссылка на self с целью избежать  Retain Cycle

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -120,9 +120,11 @@ final class MovieQuizViewController: UIViewController {
             message: result.text,
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
+            
             self.show(quiz: self.convert(model: self.questions[self.currentQuestionIndex]))
         }
         alert.addAction(action)
@@ -147,7 +149,8 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         if isCorrect { correctAnswers += 1 }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResults()
         }
     }
