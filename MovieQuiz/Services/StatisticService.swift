e3dbfd35bbd449b860842c6e17bb1522d3d72ede
Shestakov Bogdan <dreamlogin@internet.ru>
2023-12-21 04:44:11 +0300

    В процессе работа над 5 спринтом

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -1,37 +1,86 @@
 import UIKit
 
-final class StatisticServiceImplementation: StatisticService {
-    func store(correct count: Int, total amount: Int) {
-        <#code#>
-    }
-    
-    var totalAccuracy: Double
-    var gamesCount: Int
-    var bestGame: GameRecord {
-        get {
-            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
-            let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
-                return .init(correct: 0, total: 0, date: Date())
-            }
-
-            return record
-        }
-
-        set {
-            guard let data = try? JSONEncoder().encode(newValue) else {
-                print("Невозможно сохранить результат")
-                return
-            }
-
-            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
-        }
-    }
+final class StatisticServiceImplementation: StatisticServiceProtocol {
     
     private enum Keys: String {
         case correct, total, bestGame, gamesCount
     }
     
-    private var newValue = String()
     private let userDefaults = UserDefaults.standard
     
+    var totalAccuracy: Double {
+        get {
+            if total > 0 {
+                return Double(correct) / Double(total) * 100
+            } else { return 0 }
+        }
+    }
+    
+    
+    var bestGame: GameRecord {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+                  let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            
+            return record
+        }
+        
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    var gamesCount: Int {
+        get {
+            userDefaults.integer(forKey: Keys.gamesCount.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.gamesCount.rawValue)
+        }
+    }
+    
+    var correct: Int {
+        get {
+            userDefaults.integer(forKey: Keys.correct.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.correct.rawValue)
+        }
+    }
+    
+    var total: Int {
+        get {
+            userDefaults.integer(forKey: Keys.total.rawValue)
+        }
+        set {
+            userDefaults.setValue(newValue, forKey: Keys.total.rawValue)
+        }
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        gamesCount += 1
+        correct += count
+        total += amount
+        let currentGame = GameRecord(correct: count, total: amount, date: Date())
+        if currentGame.isBetterThan(bestGame) {
+            bestGame = currentGame
+        }
+    }
+    
+    //отладочная функция для сброса статистики
+    func resetStatistic() {
+        userDefaults.removeObject(forKey: Keys.correct.rawValue)
+        userDefaults.removeObject(forKey: Keys.total.rawValue)
+        userDefaults.removeObject(forKey: Keys.bestGame.rawValue)
+        userDefaults.removeObject(forKey: Keys.gamesCount.rawValue)
+    }
+    
 }
+
