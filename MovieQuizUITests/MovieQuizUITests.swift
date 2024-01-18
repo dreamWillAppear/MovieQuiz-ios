fc3efe6f5686d63deb9b4f07d21770ec2dd324aa
Shestakov Bogdan <dreamlogin@internet.ru>
2024-01-18 21:36:17 +0300

    В процессе работа над 7 спринтом

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,42 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Богдан Шестаков on 14.01.2024.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        continueAfterFailure = false
+        
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testYesButton() {
+        let firstPoster = app.images["Poster"]
+        
+        app.buttons["Yes"].tap()
+        
+        let secondPoster = app.images["Poster"]
+        
+        XCTAssertFalse(firstPoster == secondPoster)
+    }
+}
